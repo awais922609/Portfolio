228a4711ede16a166aa504aaad28f02453d179be
awais922609 <awaissajid92260@gmail.com>
2023-06-20 18:07:11 +0500

    Merge changes from remote repository

---
+++ src/components/Projects.js
@@ -0,0 +1,96 @@
+import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
+import { ProjectCard } from "./ProjectCard";
+import projImg1 from "../assets/img/forth project.jpg";
+import projImg2 from "../assets/img/second work.jpg";
+import projImg3 from "../assets/img/project1.jpg";
+import projImg4 from "../assets/img/first work.jpg";
+import projImg5 from "../assets/img/fifth work.jpg";
+import projImg6 from "../assets/img/12345.jpg";
+import colorSharp2 from "../assets/img/color-sharp2.png";
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
+
+export const Projects = () => {
+
+  const projects = [
+    {
+      
+      description: "An advanced intrusion detection system that combines static and machine learning rules, achieving a 90% accuracy rate in anomaly detection.",
+      imgUrl: projImg1,
+    },
+    {
+      description: "Developed a flexible TLS implementation supporting AES, RSA, and ECC ciphers, enabling secure communication through data encryption on both ends. Enhanced confidentiality, integrity, and customization of encryption protocols for improved data protection.",
+      imgUrl: projImg2,
+    },
+    {
+      description: "Developed a blockchain-based toll management dApp using Solidity, Truffle, and React",
+      imgUrl: projImg3,
+    },
+    {
+      description: "Conducted a website security assessment project, identifying vulnerabilities and implementing code patches to enhance security posture. This hands-on experience improved understanding of web application security and emphasized the significance of proactive measures in mitigating cyber threats.",
+      imgUrl: projImg4,
+    },
+    {
+      description: "Conducted digital forensics projects, examining hard drives, USBs, and emails to uncover evidence, trace malicious actors, and support cybersecurity initiatives, utilizing advanced techniques and tools.",
+      imgUrl: projImg5,
+    },
+    {
+      description: "Explored binary analysis for automotive systems, successfully demonstrating signal manipulation vulnerabilities through virtual hacking, highlighting risks and contributing to enhancing connected vehicle cybersecurity.",
+      imgUrl: projImg6,
+    },
+  ];
+
+  return (
+    <section className="project" id="projects">
+      <Container>
+        <Row>
+          <Col size={12}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+              <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
+                <h2>Projects</h2>
+                <p>I show you all the big Projects I have done so far</p>
+                <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                  <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+                    <Nav.Item>
+                      <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                    </Nav.Item>
+                    {/* <Nav.Item>
+                      <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="third">Tab 3</Nav.Link>
+                    </Nav.Item> */}
+                  </Nav>
+                  <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
+                    <Tab.Pane eventKey="first">
+                      <Row>
+                        {
+                          projects.map((project, index) => {
+                            return (
+                              <ProjectCard
+                                key={index}
+                                {...project}
+                                />
+                            )
+                          })
+                        }
+                      </Row>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="section">
+                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="third">
+                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
+                    </Tab.Pane>
+                  </Tab.Content>
+                </Tab.Container>
+              </div>}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+      <img className="background-image-right" src={colorSharp2}></img>
+    </section>
+  )
+}
