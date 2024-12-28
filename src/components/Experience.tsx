c7695d5f59e3d59e9b171729e20943bef5987fdd
Awais Sajid <69464258+awais922609@users.noreply.github.com>
2024-12-28 11:50:52 +0500

    commit

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -7,11 +7,11 @@ const experiences = [
     period: "Sep 2024 – present",
     location: "Islamabad, Pakistan",
     points: [
-  "- Monitored and investigated over 500 security alerts monthly using Trend Micro Server Security and Infoblox BloxOne Threat Defense, identifying and mitigating threats to ensure zero data breaches. ",
-  "- Conducted log analysis on more than 1,000 events, leading to an 80% reduction in incident response time by automating domain blocking/allowing based on company policies.",
-  "- Utilized Trend Micro XDR to detect and neutralize potential threats in real-time, reducing threat  detection time by 40%.",
-  "- Delivered technical support to 20+ customers, resolving security issues swiftly and contributing to a 95% customer satisfaction rate.",
-  "- Collaborated with cross-functional teams to integrate security tools, achieving seamless deployment with 0% operational downtime.",
+  "Monitored and investigated over 500 security alerts monthly using Trend Micro Server Security and Infoblox BloxOne Threat Defense, identifying and mitigating threats to ensure zero data breaches. ",
+  "Conducted log analysis on more than 1,000 events, leading to an 80% reduction in incident response time by automating domain blocking/allowing based on company policies.",
+  "Utilized Trend Micro XDR to detect and neutralize potential threats in real-time, reducing threat  detection time by 40%.",
+  "Delivered technical support to 20+ customers, resolving security issues swiftly and contributing to a 95% customer satisfaction rate.",
+  "Collaborated with cross-functional teams to integrate security tools, achieving seamless deployment with 0% operational downtime.",
     "Stayed updated with emerging cybersecurity trends and technologies to implement best practices."
     ]
   },
@@ -21,9 +21,9 @@ const experiences = [
     period: "Feb 2024 – Aug 2024",
     location: "Lahore, PK",
     points: [
-    "- Conducted 3+ gap and risk assessments, implementing mitigation strategies that reduced security vulnerabilities by 25%.",
-    "- Led ISO 27001, ISO 27701 compliance initiatives, ensuring the company met global standards and reducing non-compliance incidents by 15%.",
-    "- Assisted in SSAE-18 and PCI audits, contributing to a successful audit outcome."
+    "Conducted 3+ gap and risk assessments, implementing mitigation strategies that reduced security vulnerabilities by 25%.",
+    "Led ISO 27001, ISO 27701 compliance initiatives, ensuring the company met global standards and reducing non-compliance incidents by 15%.",
+    "Assisted in SSAE-18 and PCI audits, contributing to a successful audit outcome."
 
     ]
   },
@@ -34,9 +34,9 @@ const experiences = [
     location: "Islamabad, Pakistan",
     points: [
       
-      "- Managed and optimized 500+ user laptops and devices, ensuring a 98% uptime and enhancing user satisfaction.",
-      "- Assisted in deploying Rapid7 agents, enhancing system security across 100+ devices.",
-      "- Updated security policies for compliance with industry standards."
+      "Managed and optimized 500+ user laptops and devices, ensuring a 98% uptime and enhancing user satisfaction.",
+      "Assisted in deploying Rapid7 agents, enhancing system security across 100+ devices.",
+      "Updated security policies for compliance with industry standards."
     ]
   }
 ];
